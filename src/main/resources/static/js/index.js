9d8567d7d57b43ba7f686e9d8b7d8b16830ff563
YordanKarpachev <yordkarp@icloud.com>
2024-03-11 19:52:47 +0100

    railway test

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,6 +1,6 @@
 //index.js
 
-import { updateModels , updateYear} from "./common.js";
+
 
 
 
@@ -57,3 +57,67 @@ function updateSearchBtn(data){
 
 }
 
+function updateModels(brandSelectId, modelSelectId, endpointBase) {
+  
+
+  
+  
+  const brandSelect = document.getElementById(brandSelectId);
+  const modelSelect = document.getElementById(modelSelectId);
+
+  brandSelect.addEventListener('change', function () {
+      const selectedBrand = this.value;
+
+      const endpoint = `${endpointBase}${encodeURIComponent(selectedBrand)}/models`;
+
+      fetch(endpoint)
+          .then(response => response.json())
+          .then(models => {
+              modelSelect.innerHTML = '<option value="">- Select a model -</option>';
+              models.forEach(model => {
+                  let option = document.createElement('option');
+                  option.value = model;
+                  option.textContent = model;
+                  modelSelect.appendChild(option);
+              });
+
+                  updateYear();
+          })
+          .catch(error => console.error('Error fetching models:', error));
+  });
+}
+
+function updateYear(){
+  const modelSelect = document.getElementById('model');
+
+  modelSelect.addEventListener('change', function(){
+  
+  const selectedModel = modelSelect.value;
+  
+
+
+  fetch(`/api/getModelStartYear/${encodeURIComponent(selectedModel)}`)
+
+ 
+      .then(response => response.json())
+      .then(data => {
+
+       
+          const currentYear = new Date().getFullYear();
+          const yearSelect = document.getElementById('year');
+          yearSelect.innerHTML = '<option value="">- Select a year -</option>';
+          
+          for(let year = data ; year <= currentYear ;year++){
+              let option = document.createElement('option');
+              option.value = year;
+              option.text = year;
+              yearSelect.appendChild(option)
+          }
+
+
+      })
+
+      .catch(error => console.error('Error fetching model start year:', error));
+  
+})
+}
